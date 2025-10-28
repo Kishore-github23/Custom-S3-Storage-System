2f2d0c860f5925eeb620688c4ffd98b3368e9942
kishorekumarjothikumar@gmail.com <kishorekumarjothikumar@gmail.com>
2025-10-28 13:51:05 +0530

    Updating spring project from eclipse.

---
+++ s3-storage/s3-storage/src/main/java/com/storage/s3/S3StorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.storage.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3StorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3StorageSystemApplication.class, args);
+	}
+
+}
---
+++ s3-storage/s3-storage/src/test/java/com/storage/s3_storage/S3StorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage.s3_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3StorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
