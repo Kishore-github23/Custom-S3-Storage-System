318a3d31cf155cb720dfc68e91c75976bfcef228
kishorekumarjothikumar@gmail.com <kishorekumarjothikumar@gmail.com>
2025-11-11 12:33:30 +0530

    Finally resolved the issue, root cause this main.ts file were not using config.ts file where auth-interceptor called.

--- s3-storage/s3-storage-frontend/src/main.ts
+++ s3-storage/s3-storage-frontend/src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
 
@@ -10,9 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
